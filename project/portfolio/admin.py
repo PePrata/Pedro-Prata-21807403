223a01d7ea6b8e5332ad33282609925846b2c165
PePrata-a21807403 <116282085+PePrata@users.noreply.github.com>
2026-04-06 15:52:00 +0000

    Class Licenciatura

--- project/portfolio/admin.py
+++ project/portfolio/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Licenciatura
+
 # Register your models here.
+
+class LicenciaturaAdmin(admin.ModelAdmin):
+    list_display = ("nome", "descricao")
+    search_fields = ("nome",)
+
+admin.site.register(Licenciatura, LicenciaturaAdmin)
--- project/portfolio/models.py
+++ project/portfolio/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Licenciatura(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao = models.TextField(blank=True)
